6f463b994b478bb2ce17d716903a29409f2d9e65
DevJosL <jmoran-2022388@kinal.edu.gt>
2026-04-15 12:04:07 -0600

    Test

--- src/app/router/AppRoutes.jsx
+++ src/app/router/AppRoutes.jsx
@@ -1,17 +1,24 @@
-import {Route, Routes} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthPage } from "../../features/auth/pages/AuthPage.jsx";
 import { DashboardPage } from "../layouts/DashboardPage.jsx";
 
-export const AppRoutes = ()=>{
+export const AppRoutes = ()=> {
 
     return(
         <Routes>
-            {/* PÚBLICAS */}
-            <Route path="/" element={<AuthPage />} />
 
-            {/* PROTEGIDO POR ROLE */}
-            <Route path="/dashboard/*" element={<DashboardPage />} />
+            {/* PUBLIC */}
+            <Route path="/" element={<AuthPage/>} />
 
+
+            {/* PROTECTED + ROLE */}
+            <Route
+                path="/dashboard/*" element={<DashboardPage />}
+            />
+
+
+            {/* Ruta temporal para pruebas */}
+            <Route path="*" element={<h1>Página no encontrada</h1>} />
         </Routes>
     );
 }
--- src/features/auth/components/ForgotPasswordForm.jsx
+++ src/features/auth/components/ForgotPasswordForm.jsx
@@ -2,27 +2,30 @@ export const ForgotPasswordForm = ({onSwitch}) => {
     return (
         <form className="space-y-5">
             <div>
-                <label className="block text-sm font-medium text-gray-800 mb-1.5">
+                <label className="block text-sm font-medium text-gray-300">
                     Email
                 </label>
                 <input
                     type="email"
                     placeholder="correo@ejemplo.com"
-                    className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                    className="w-full px-3 py-2.5 mb-5 text-sm bg-gray-700 border border-gray-600 text-white 
+                    placeholder:text-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-400 "
                 />
                 <button
                     type="submit"
-                    className="w-full bg-main-blue text-white py-2.5 px-4 rounded-lg text-sm font-medium hover:opacity-90 transition"
+                    className="w-full mb-3 bg-gradient-to-r from-yellow-500 to-yellow-400 hover:from-yellow-400 
+                    hover:to-yellow-300 text-gray-900 font-semibold py-2.5 px-4 rounded-lg transition-all 
+                    duration-200 text-sm"
                 >
                 Enviar correo
                 </button>
-                    <p className="text-center text-sm text-gray-600">
+                    <p className="text-center text-sm text-gray-400">
                     ¿Recordaste tu contraseña?{" "}                
                 </p>
                 <button
                     type="button"
                     onClick={onSwitch}
-                    className="text-main-blue font-medium hover:underline"
+                    className="text-yellow-400 hover:text-yellow-300 hover:underline mx-auto block"
                 >
                     Iniciar sesión
                 </button>
